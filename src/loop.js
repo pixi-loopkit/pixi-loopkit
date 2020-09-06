16fc0a842941b8c7f29021d333ef4721e4512e53
Tom Striker <toms.baugis@gmail.com>
2020-09-06 17:40:14 +0100

    loop - add a loops counter so that we can progress on each loop in whichever sense

--- src/loop.js
+++ src/loop.js
@@ -2,6 +2,7 @@ class Loop {
     constructor(frames = 120) {
         this._frames = frames;
         this.frameFull = 0; // goes from 0 to frames
+        this.loops = 0; // counter for total loops performed
     }
 
     get frames() {
@@ -23,6 +24,10 @@ class Loop {
         return this.norm;
     }
 
+    set frame(frame) {
+        this.frameFull = this._frames * frame;
+    }
+
     zig(times = 2) {
         // splits frame's 0->1 into 0->1->0->1->0->1, N times.
         // times=1 matches input, times=2 will render 0->1->0, times=3 will render 0->1->0->1, etc
@@ -37,7 +42,7 @@ class Loop {
         return this.zig(times * 2);
     }
 
-    fullCycle(callback) {
+    fullCircle(callback) {
         // cycles through the full loop and makes sure we are not missing any frames, nor we do have any extra
         for (let frameFull = 0; frameFull < this.frames; frameFull++) {
             this.frameFull = frameFull;
@@ -59,10 +64,18 @@ class Loop {
     }
 
     tick(frames) {
-        let nextFrame = this.frameFull + (frames || 1);
+        let prevFrame = this.frameFull;
+        let nextFrame = prevFrame + (frames || 1);
         // while normally you'd loop 0..last, we loop 0..last-1 as the final frame should be the same as first frame
         let totalFrames = this.frames - 1;
-        this.frameFull = (totalFrames + nextFrame) % totalFrames;
+        nextFrame = (totalFrames + nextFrame) % totalFrames;
+
+        let forward = (frames || 1) > 0;
+        if ((forward && prevFrame > nextFrame) || (!forward && prevFrame < nextFrame)) {
+            this.loops += forward ? 1 : -1;
+        }
+
+        this.frameFull = nextFrame;
     }
 }
 
